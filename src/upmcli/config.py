436140d4a2c323bdc1bb8b5ba894a974a1d03102
farhoud <farhoud.m7@gmail.com>
2018-01-07 12:15:32 +0330

    - a config module for Global variable and configurations

---
+++ src/upmcli/config.py
@@ -0,0 +1,8 @@
+import logging
+
+LOGGING_LEVEL = logging.DEBUG
+SPEC_FILE_NAME = 'upm.yml'
+MAIN_DIR = '.upm'
+ENTRY_INDEX_FILE_NAME = 'entrypoints.yml'
+NETWORK_DETAILS_FILE_NAME = 'network.yml'
+CONTAINER_VOLUME_MOUNT_PATH = '/src/'
