b99d9fa2937dcc9d97cfbac080a91b8ae80817c5
farhoud <farhoud.m7@gmail.com>
2018-01-07 16:00:25 +0330

    - move upx package - move publish and install package to package specification module - remove actions - use click group and and sub command instead of action dict - add --debug for enabling debugging logs (it need improvement)

--- src/upmcli/package_specification.py
+++ src/upmcli/package_specification.py
@@ -1,9 +1,13 @@
 import os
 import logging
 import click
+import docker
 import yaml
+from git import Repo, InvalidGitRepositoryError
+from upmcli.utils import file_path_generator, image_name, log_output
 from upmcli.entrypoints_index import EntrypointsIndex
 from upmcli.config import *
+
 
 class PackageSpecification:
     def __init__(self, yml_path, name, author, version, description, entrypoints, docker=None, dependencies=None):
@@ -44,7 +48,7 @@ class PackageSpecification:
         return self.yml_path
 
 
-def from_prompt(args):
+def from_prompt():
     logging.info('loading yml')
     cwd = os.getcwd()
     logging.info(cwd)
@@ -98,3 +102,41 @@ def from_file(file_path):
     from upmcli.utils import load_yml
     yml_obj = load_yml(file_path)
     return PackageSpecification(yml_path=file_path, **yml_obj)
+
+
+def install_package(path):
+    # @Todo check if args[0] is a path to a yml file or a package name
+    # @Todo check docker path in upm yml and convert it to absolute path
+    installing_pkg_path = path
+    curr_pkg_spec = from_file(file_path_generator(os.getcwd()))
+    logging.debug('current pkg: ' + str(curr_pkg_spec))
+    installing_pkg_spec = from_file(file_path_generator(installing_pkg_path))
+    logging.debug('installing pkg: ' + str(installing_pkg_spec))
+    logging.debug('docker value in installing pkg:' + str(installing_pkg_spec.docker))
+    client = docker.api.APIClient(base_url='unix://var/run/docker.sock')
+    if installing_pkg_spec.docker:
+        try:
+            docker_image_name = image_name(curr_pkg_spec.name, installing_pkg_spec.name, curr_pkg_spec.version)
+            gen = client.build(path=installing_pkg_path,
+                               rm=True,
+                               tag=docker_image_name)
+            log_output(gen)
+            curr_pkg_spec.add_dep(str(installing_pkg_spec.name).lower().replace(' ', '_'))
+            logging.debug('package path is :' + installing_pkg_path)
+            curr_pkg_spec.save_to_file()
+            e_index = EntrypointsIndex.from_file(path=os.path.join(os.getcwd(), MAIN_DIR, ENTRY_INDEX_FILE_NAME))
+            # @TODO do sth for entrypoint duplication
+            e_index.add_entrypoints(installing_pkg_spec.entrypoints, docker_image_name)
+            e_index.save_to_file()
+        except Exception as e:
+            logging.error('ERROR')
+            print(e)
+
+
+def publish_package():
+    click.echo("project most be git repo and have public remote, which the branch u wish ")
+    try:
+        repo = Repo(search_parent_directories=True)
+
+    except InvalidGitRepositoryError:
+        logging.error("this is not git repository please init git and with public remote before start publishing")
